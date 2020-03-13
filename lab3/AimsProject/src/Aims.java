b0b1d1b098ebd7a55bc3951d7d3e1e7156aadc74
Le Hoang Anh Trung <trung.lha176892@sis.hust.edu.vn>
2020-03-13 23:35:45 +0700

    update lab3

---
+++ lab3/AimsProject/src/Aims.java
@@ -0,0 +1,7 @@
+
+public class Aims {
+	public static void main(String[] args){
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("dvd1","category1");
+		System.out.println(dvd1.getCategory());
+	}
+}
